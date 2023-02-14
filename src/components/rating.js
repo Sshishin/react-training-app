eeaddac60d55aaa0b520e7f55a243f0c5633577f
Sergey Shishin <flyers32rus@gmail.com>
2023-02-14 22:06:28 +0300

    feat: import/export

---
+++ src/components/rating.js
@@ -0,0 +1,15 @@
+import '../components/rating.css'
+
+function Rating() {
+    return (
+        <div className="rating-flex">
+            <div className='star'></div>
+            <div className='star'></div>
+            <div className='star'></div>
+            <div className='star'></div>
+            <div className='star'></div>
+        </div>
+    )
+}
+
+export default Rating;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Rating from './components/rating';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Rating />
   </React.StrictMode>
 );
 
