0a5b58da59585c15da4f4feca4aeb005026a4ec7
Sergey Shishin <flyers32rus@gmail.com>
2023-03-02 22:37:00 +0300

    feat: managed component

--- src/Counter.js
+++ src/Counter.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 function Counter() {
     const [state, setState] = useState(2)
+    const [value, setValue] = useState('Двустороннее связывание')
 
     function increment() {
         setState(state + 1)
@@ -17,6 +18,8 @@ function Counter() {
             <h1>{state}</h1>
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
+            <h2>{value}</h2>
+            <input value={value} onChange={e => setValue(e.target.value)}></input>
         </div>
     )
 }
